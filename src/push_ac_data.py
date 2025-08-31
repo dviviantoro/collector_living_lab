7fb95bb7e335fbc504a8191235ec857d1fc7bcf1
dviviantoro <deny.viviantoro@gmail.com>
2025-08-31 19:23:21 +0700

    minor11

--- src/push_ac_data.py
+++ src/push_ac_data.py
@@ -50,8 +50,8 @@ for message in pubsub.listen():
             with open(temp_json, 'r') as json_file:
                 data_dict = json.load(json_file)
                 print(data_dict)
-            #     push_data(data_dict)
-            # os.remove(temp_json)
+                push_data(data_dict)
+            os.remove(temp_json)
 
-            # time.sleep(1)
-            # ac_channels = {key: False for key in ac_channels}
+            time.sleep(1)
+            ac_channels = {key: False for key in ac_channels}
